12098e98101e97c5f4f21ff6f7bbb5bffe53f80a
wataru0531 <obito0531@gmail.com>
2021-03-10 01:25:42 +0900

    アップデートしました。

--- scripts/libs/smooth.js
+++ scripts/libs/smooth.js
@@ -12,14 +12,14 @@ class Smooth{
     _addEvent(){
         for(let i = 0; i < this.DOM.smoothScrollTrigger.length; i++){
             this.DOM.smoothScrollTrigger[i].addEventListener(this.eventType, function(e){
-                e.preventDefault(); //デフォルトのaタグの動作をキャンセル。これを書かないとスムーススクロール無しでジャンプする。
+                e.preventDefault();
     
-                let href = this.getAttribute('href'); //hrefの値を取得する。例：#features
-                let targetElement = document.getElementById(href.replace('#', '')); //replaceで#を除く。例：features
+                let href = this.getAttribute('href'); 
+                let targetElement = document.getElementById(href.replace('#', '')); 
     
-                const rect = targetElement.getBoundingClientRect().top;  //プラウザの上からtargetElement上までの高さを取得
-                const offset = window.pageYOffset;  //現在のスクロール量を取得
-                const headerHeight = 94;  //ヘッダーの高さを取得。fixedを指定している場合やheaderを浮かしている場合のみ必要。
+                const rect = targetElement.getBoundingClientRect().top;
+                const offset = window.pageYOffset;
+                const headerHeight = 94; 
                 const target = rect + offset - headerHeight;//
     
                 window.scrollTo({
